313b2c2caba5216c0eb4721eaa9c18ce141f259a
homoarea <homoarea114@gmail.com>
2025-04-15 19:13:29 +0800

    basic function completed!

---
+++ README.md
@@ -0,0 +1,14 @@
+# hentai
+**hentai** is a simple cli for view e-hentai.org
+
+## Quickstart
+### Using cli
+```
+pip install hentai
+
+hentai --help
+```
+### python code
+```
+form hentai import fetch
+```
---
+++ setup.py
@@ -0,0 +1,41 @@
+import os
+from codecs import open
+from setuptools import setup
+
+requires=[
+    "click",
+    "requests",
+    "chardet",
+    "beautifulsoup4",
+    "lxml",
+]
+
+about={}
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here,'src','hentai','__version__.py'),'r','utf-8') as f:
+    exec(f.read(),about)
+print(about)
+
+setup(
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__description__"],
+    author=about["__author__"],
+    author_email=about["__author_email__"],
+    url=about["__url__"],
+    license=about["__license__"],
+
+    packages=["hentai"],
+    package_dir={"":"src"},
+    package_data={"":["LICENSE","NOTICE"]},
+    include_package_data=True,
+    python_requires=">=3.8",
+    install_requires=requires,
+    entry_points="""
+        [console_scripts]
+        eh=hentai.eh:cli
+    """,
+    project_urls={
+        "Source":"https://github.com/homoarea/hentai"
+    }
+)
