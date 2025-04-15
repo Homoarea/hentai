c59025dae1114d08547c26fc452bda9b58e1400b
homoarea <homoarea114@gmail.com>
2025-04-15 20:49:57 +0800

    move html results

--- t2.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-from codecs import open
-about={}
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here,'src','hentai','__version__.py'),'r','utf-8') as f:
-    exec(f.read(),about)
-print(about)
