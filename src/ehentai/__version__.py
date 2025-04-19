fdb816d93b57d7fdc4867310967b1cff81d316da
homoarea <homoarea114@gmail.com>
2025-04-19 19:45:38 +0800

    upload 0.2.1

--- src/ehentai/__version__.py
+++ src/ehentai/__version__.py
@@ -8,7 +8,7 @@
 """
 __title__="ehentai"
 __description__="Python for viewing e-hentai"
-__version__="0.2"
+__version__="0.2.1"
 __url__="https://github.com/Homoarea/hentai"
 __build__=0x3f3f3f
 __author__="Homoarea"
